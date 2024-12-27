ed207238306f32fbbca13a6cb0ce0fe65b515ad2
showrov4g <showrovg00@gmail.com>
2024-12-27 19:09:05 +0600

    page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router'
+import Home from './Pages/Home'
+import Doctors from './Pages/Doctors'
+import Login from './Pages/Login'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import MyProfile from './Pages/MyProfile'
+import MyAppointments from './Pages/MyAppointments'
+import Appointment from './Pages/Appointment'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='mx-4 sm:mx-[10%]'>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/doctors' element={<Doctors/>} />
+          <Route path='/doctors/:specialty' element={<Doctors/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='/my-profile' element={<MyProfile/>} />
+          <Route path='/my-appointment' element={<MyAppointments/>} />
+          <Route path='/appointment/:docId' element={<Appointment/>} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
