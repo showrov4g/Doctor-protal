2684e4809960916d63147ccdadf5e38fa97bbb60
showrov4g <showrovg00@gmail.com>
2024-12-27 23:11:57 +0600

    all header content setup

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Header = () => {
-    return (
+  return (
+    <div>
+      {/* ==================left side ============== */}
+      <div>
+        <p>
+          Book Appointment <br />
+          With Trusted Doctors
+        </p>
         <div>
-            
+            <img src={assets.group_profiles} alt="" />
+            <p>Simply browse through our extensive list of trusted doctors, 
+            schedule your appointment hassle-free.</p>
         </div>
-    );
+        <a href="">
+        Book appointment <img src={assets.arrow_icon} alt="" />
+        </a>
+      </div>
+      {/*================== right side ================ */}
+      <div>
+        <img src={assets.header_img} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
