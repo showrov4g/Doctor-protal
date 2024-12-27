5b3e45fe2e75055d465bf297f588baf63a85bd31
showrov4g <showrovg00@gmail.com>
2024-12-27 22:50:20 +0600

    header section design start

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import { NavLink, useNavigate } from "react-router";
 const Navbar = () => {
   const navigate = useNavigate();
   const [showMenu, setShowMenu] = useState(false);
-  const [token, setToken] = useState(true);
+  const [token, setToken] = useState(false);
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
       <img className="w-44 cursor-pointer" src={assets.logo} alt="" />
@@ -36,7 +36,7 @@ const Navbar = () => {
                 <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
                     <p onClick={()=>navigate('/my-profile')} className="hover:text-black cursor-pointer">My Profile</p>
                     <p onClick={()=>navigate('/my-appointments')} className="hover:text-black cursor-pointer">My Appointments</p>
-                    <p className="hover:text-black cursor-pointer">Logout</p>
+                    <p onClick={()=>setToken(false)} className="hover:text-black cursor-pointer">Logout</p>
                 </div>
             </div>
           </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+
+import Header from '../Components/Header';
 
 const Home = () => {
     return (
         <div>
-            <h1>Home Page </h1>
+            <Header/>
         </div>
     );
 };
