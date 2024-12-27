4b1d63a696b04a0fda1f2f8e9e548cd393bac34e
showrov4g <showrovg00@gmail.com>
2024-12-27 21:37:50 +0600

    state variable set

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {assets} from "../assets/assets"
 import { NavLink, useNavigate } from 'react-router';
 
 const Navbar = () => {
     const navigate = useNavigate()
+    const [showMenu, setShowMenu] = useState(false);
+    const [token, setToken] = useState(true)
     return (
         <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
             <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
