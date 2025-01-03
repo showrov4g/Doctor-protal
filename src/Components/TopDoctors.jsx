cc768ac1a3aca85c632dbcb1ce8b441bb1fdcd8c
showrov4g <showrovg00@gmail.com>
2025-01-03 23:29:34 +0600

    Top doctors sections design

--- src/Components/TopDoctors.jsx
+++ src/Components/TopDoctors.jsx
@@ -1,9 +1,29 @@
+import { Link } from "react-router";
+import { doctors } from "../assets/assets";
 
 
 const TopDoctors = () => {
     return (
-        <div>
-            
+        <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
+            <h1 className="text-3xl font-medium ">Top Doctors to Book</h1>
+            <p className="sm:w1/3 text-center text-sm">Simply browse through our extensive list of trusted doctors.</p>
+            <div>
+                {
+                    doctors?.slice(0, 10)?.map((item, index)=>(
+                        <div key={index}>
+                            <img src={item.image} alt="" />
+                            <div>
+                                <div>
+                                    <p></p><p>Available</p>
+                                </div>
+                                <p>{item.name}</p>
+                                <p>{item.speciality}</p>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+            <button>more</button>
         </div>
     );
 };
