62482f672d24ba3dd29fb8c8b70792566f993b53
showrov4g <showrovg00@gmail.com>
2025-01-03 22:31:17 +0600

    section section  is complete

--- src/Components/SpecificityMenu.jsx
+++ src/Components/SpecificityMenu.jsx
@@ -2,16 +2,16 @@ import { specialityData } from "../assets/assets";
 import { Link } from "react-router";
 const SpecificityMenu = () => {
   return (
-    <div id="specialty">
-      <h1>Find by Speciality </h1>
-      <p>
+    <div id="specialty" className="flex flex-col items-center gap-4 py-16 text-gray-800">
+      <h1 className="text-3xl font-medium ">Find by Speciality </h1>
+      <p className="w-1/3 text-center text-sm">
         Simply browse through our extensive list of trusted doctors, schedule
         your appointment hassle-free.
       </p>
-      <div>
+      <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll ">
         {specialityData?.map((item, index) => (
-          <Link key={index} to={`/doctors/${item.speciality}`}>
-            <img src={item.image} alt="" />
+          <Link className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500" key={index} to={`/doctors/${item.speciality}`}>
+            <img className="w-16 sm-14 mb-2" src={item.image} alt="" />
             <p>{item.speciality}</p>
           </Link>
         ))}
