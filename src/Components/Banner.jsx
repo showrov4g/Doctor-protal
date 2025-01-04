24087543dd7cbc3311f37bc41f0930b2063e8beb
showrov4g <showrovg00@gmail.com>
2025-01-04 11:14:12 +0600

    add banner content

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,10 +1,22 @@
+import { assets } from "../assets/assets";
 
 const Banner = () => {
-    return (
+  return (
+    <div>
+      {/* left side  */}
+      <div>
         <div>
-            
+          <p>Book Appointment </p>
+          <p>With 100+ Trusted Doctors</p>
         </div>
-    );
+        <button>Create account</button>
+      </div>
+      {/* right side  */}
+      <div>
+        <img src={assets.appointment_img} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
