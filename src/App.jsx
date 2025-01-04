741b11ee0e674405b09605dc4e94a503bec6d2eb
showrov4g <showrovg00@gmail.com>
2025-01-04 12:28:37 +0600

    footer section design start

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,37 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import { Route, Routes } from 'react-router'
-import Home from './Pages/Home'
-import Doctors from './Pages/Doctors'
-import Login from './Pages/Login'
-import About from './Pages/About'
-import Contact from './Pages/Contact'
-import MyProfile from './Pages/MyProfile'
-import MyAppointments from './Pages/MyAppointments'
-import Appointment from './Pages/Appointment'
-import Navbar from './Components/Navbar'
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import { Route, Routes } from "react-router";
+import Home from "./Pages/Home";
+import Doctors from "./Pages/Doctors";
+import Login from "./Pages/Login";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import MyProfile from "./Pages/MyProfile";
+import MyAppointments from "./Pages/MyAppointments";
+import Appointment from "./Pages/Appointment";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 function App() {
-
   return (
     <>
-      <div className='mx-4 sm:mx-[10%]'>
-        <Navbar/>
+      <div className="mx-4 sm:mx-[10%]">
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/doctors' element={<Doctors/>} />
-          <Route path='/doctors/:specialty' element={<Doctors/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/about' element={<About/>} />
-          <Route path='/contact' element={<Contact/>} />
-          <Route path='/my-profile' element={<MyProfile/>} />
-          <Route path='/my-appointment' element={<MyAppointments/>} />
-          <Route path='/appointment/:docId' element={<Appointment/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/doctors" element={<Doctors />} />
+          <Route path="/doctors/:specialty" element={<Doctors />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/my-profile" element={<MyProfile />} />
+          <Route path="/my-appointment" element={<MyAppointments />} />
+          <Route path="/appointment/:docId" element={<Appointment />} />
         </Routes>
+        <Footer></Footer>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,45 @@
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="md:mx-10 ">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        {/* left side */}
+        <div>
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
+        {/* middle side */}
+        <div>
+          <p>COMPANY</p>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        {/* right side */}
+        <div>
+          <p>GET IN TOUCH</p>
+          <ul>
+            <li>+1-212-456-7890</li>
+            <li>greatstackdev@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        {/* copyright text */}
+        <hr />
+        <p>Copyright © 2024 GreatStack - All Right Reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
