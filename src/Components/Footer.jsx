af8db298a13d88c5e8f4db8619084507629b60d8
showrov4g <showrovg00@gmail.com>
2025-01-07 21:45:48 +0600

    start in new day'

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         {/* left side */}
         <div>
-          <img src={assets.logo} alt="" />
-          <p>
+          <img className="mb-5 w-40" src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
@@ -16,8 +16,8 @@ const Footer = () => {
         </div>
         {/* middle side */}
         <div>
-          <p>COMPANY</p>
-          <ul>
+          <p className="text-xl font-medium mb-5 ">COMPANY</p>
+          <ul className="flex flex-col gap-2 text-gray-600 ">
             <li>Home</li>
             <li>About us</li>
             <li>Contact us</li>
@@ -26,8 +26,8 @@ const Footer = () => {
         </div>
         {/* right side */}
         <div>
-          <p>GET IN TOUCH</p>
-          <ul>
+          <p className="text-xl font-medium mb-5 ">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-2 text-gray-600 ">
             <li>+1-212-456-7890</li>
             <li>greatstackdev@gmail.com</li>
           </ul>
@@ -36,7 +36,7 @@ const Footer = () => {
       <div>
         {/* copyright text */}
         <hr />
-        <p>Copyright © 2024 GreatStack - All Right Reserved.</p>
+        <p className="py-5 text-sm text-center">Copyright © 2024 GreatStack - All Right Reserved.</p>
       </div>
     </div>
   );
