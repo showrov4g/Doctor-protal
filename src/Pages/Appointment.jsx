783dabf14ef12260079766b07bae063b6f801839
showrov4g <showrovg00@gmail.com>
2025-01-08 09:25:02 +0600

    Appoinment page design

--- src/Pages/Appointment.jsx
+++ src/Pages/Appointment.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { AppContext } from "../Context/AppContext";
 
 const Appointment = () => {
+    const {docId}= useParams() 
+    const {doctors} = useContext(AppContext);
+    const [docInfo, setDocInfo]= useState(null)
+    const fetchDocInfo = async()=>{
+        const doctorInfo = doctors?.find(doc=>doc._id===docId)
+        setDocInfo(doctorInfo);
+    }
+    useEffect(()=>{
+        
+    },[])
     return (
         <div>
             
