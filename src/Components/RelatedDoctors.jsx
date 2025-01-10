1b40c8f5753b5c14e01f34046ba4e02d269458fb
showrov4g <showrovg00@gmail.com>
2025-01-11 01:27:38 +0600

    about us page design

--- src/Components/RelatedDoctors.jsx
+++ src/Components/RelatedDoctors.jsx
@@ -23,7 +23,7 @@ const RelatedDoctors = ({ docId, specialty }) => {
       <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
         {relDoc?.slice(0,5)?.map((item, index) => (
           <div
-            onClick={() => navigate(`/appointment/${item?._id}`)}
+            onClick={() => {navigate(`/appointment/${item?._id}`); scrollTo(0,0)}}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
             key={index}
           >
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { assets } from '../assets/assets';
 
 const About = () => {
     return (
         <div>
-            
+            <div className='text-center text-2xl pt-0 text-gray-500'>
+                <p>About <span className='text-gray-700 font-medium'>Us</span></p>
+            </div>
+            <div className=' my-10 flex flex-col md:flex-row gap-12'>
+                <img src={assets.about_image} alt="" />
+                <div>
+                    <p>Welcome to Prescripto, your trusted partner in managing your healthcare needs conveniently and efficiently. At Prescripto, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
+                    <p>Prescripto is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, Prescripto is here to support you every step of the way.</p>
+                    <b>Our Vision</b>
+                    <p>Our vision at Prescripto is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you to access the care you need, when you need it.</p>
+                </div>
+            </div>
         </div>
     );
 };
