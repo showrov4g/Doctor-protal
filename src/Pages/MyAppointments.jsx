1834e50834348167bce7d0518549f185bd2377db
showrov4g <showrovg00@gmail.com>
2025-01-11 23:09:54 +0600

    my appointmet page design

--- src/Pages/MyAppointments.jsx
+++ src/Pages/MyAppointments.jsx
@@ -1,13 +1,37 @@
+import { useContext } from "react";
+import { AppContext } from "../Context/AppContext";
 
 const MyAppointments = () => {
-    return (
-        <div>
-            <p>My appointments</p>
+  const { doctors } = useContext(AppContext);
+  return (
+    <div>
+      <p>My appointments</p>
+      <div>
+        {doctors?.slice(0, 2).map((item, index) => (
+          <div key={index}>
             <div>
-                
+              <img src={item.image} alt="" />
             </div>
-        </div>
-    );
+            <div>
+              <p>{item.name}</p>
+              <p>{item.speciality}</p>
+              <p>Address:</p>
+              <p>{item.address.line1}</p>
+              <p>{item.address.line2}</p>
+              <p>
+                <span>Date & Time:</span> 25, July, 2024 | 8:30 PM
+              </p>
+            </div>
+            <div></div>
+            <div>
+              <button></button>
+              <button></button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyAppointments;
