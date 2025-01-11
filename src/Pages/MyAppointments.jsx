3dd9283f5e80ff205fda0cab491340a63fa28641
showrov4g <showrovg00@gmail.com>
2025-01-11 22:46:11 +0600

    my appoinment page digine start

--- src/Pages/MyAppointments.jsx
+++ src/Pages/MyAppointments.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
 
 const MyAppointments = () => {
     return (
         <div>
-            
+            <p>My appointments</p>
+            <div>
+                
+            </div>
         </div>
     );
 };
